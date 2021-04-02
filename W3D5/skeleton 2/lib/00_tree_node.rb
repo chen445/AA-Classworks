f6e80a6393b661b400d08a48ffbd3c8e56847b1c
Brenda J <linjiangda@yahoo.com>
2021-04-02 17:53:16 -0400

    Created more instance methods

--- W3D5/skeleton 2/lib/00_tree_node.rb
+++ W3D5/skeleton 2/lib/00_tree_node.rb
@@ -7,15 +7,45 @@ class PolyTreeNode
         @value = value
     end
 
-    def parent=(node)
+    def parent=(node)       # node2.parent=(node1= 'root')
         if node == nil
             @parent = nil 
         else  
             if @parent != nil
-                @parent.children.delete(self)
+                @parent.children.delete(self)   # node2.parent
             end
             @parent = node
             node.children << self if !node.children.include?(self)
         end 
     end
+
+    def add_child(child_node)       # node2  == self .add_children(node3)
+        if !self.children.include?(child_node)
+            child_node.parent=(self)
+        end
+    end
+
+    def remove_child(child_node)        #node2.remove_child(node3)
+        if !self.children.include?(child_node)
+        raise "Node is not a child"
+        end
+        child_node.parent=(nil)
+    end
+
+
+
+    
+    def dfs(target_value)
+        return self if self.value == target_value
+         
+        self.children.each do |child|
+            result = child.dfs(target_value)
+            if result != nil
+                return result
+            end
+        end
+        nil
+    end
+
+
 end
