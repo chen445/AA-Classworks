8c75656ec0cd0a5ee04def4d235e8b9ddf1c9c1e
Ying <cy240705@gmail.com>
2021-04-02 13:42:58 -0700

    calsswork

--- W3D5/skeleton 2/lib/00_tree_node.rb
+++ W3D5/skeleton 2/lib/00_tree_node.rb
@@ -1,3 +1,21 @@
 class PolyTreeNode
+    attr_reader :children, :parent , :value
+    attr_writer :parent
+    def initialize(value)
+        @children = []
+        @parent = nil
+        @value = value
+    end
 
+    def parent=(node)
+        if node == nil
+            @parent = nil 
+        else  
+            if @parent != nil
+                @parent.children.delete(self)
+            end
+            @parent = node
+            node.children << self if !node.children.include?(self)
+        end 
+    end
 end
