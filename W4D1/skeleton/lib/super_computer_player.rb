776a68057bef8097ff73377fc83b71360896b01b
Ying <cy240705@gmail.com>
2021-04-05 17:17:05 -0700

    finished

--- W4D1/skeleton/lib/super_computer_player.rb
+++ W4D1/skeleton/lib/super_computer_player.rb
@@ -1,7 +1,24 @@
 require_relative 'tic_tac_toe_node'
-
 class SuperComputerPlayer < ComputerPlayer
+  attr_reader :node, :mark
   def move(game, mark)
+    @node=TicTacToeNode.new(game.board,mark)
+    @mark= mark
+    node.children.each do |child|
+      if child.winning_node?(mark)
+        return child.prev_move_pos
+      else 
+        nil
+      end 
+    end 
+    node.children.each do |child|
+        if !child.losing_node?(mark)
+        return child.prev_move_pos
+        else 
+          nil 
+        end 
+    end 
+      raise "There are no non-losing nodes."
   end
 end
 
