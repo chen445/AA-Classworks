2114e38b68b3843a29f848fa0b1358f2b7a78354
Ying <cy240705@gmail.com>
2021-04-26 13:57:42 -0700

    create session view

--- W7D1/skeleton/app/controllers/sessions_controller.rb
+++ W7D1/skeleton/app/controllers/sessions_controller.rb
@@ -7,11 +7,19 @@ class SessionsController < ApplicationController
     def create
         user = User.find_by_credentials(params[:user][:user_name], params[:user][:password])
         if user
-            login!(user)
-            redirect_to user_url(user)
+            login(user)
+
+            redirect_to cats_url
         else
             render :new
         end
     end
+
+
+    def destroy
+    
+            
+    end 
+
 
 end
