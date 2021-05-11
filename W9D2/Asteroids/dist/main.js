74b3b9b364fce0b1de101620a72a3091e8232736
moustafa <mogarcia626@gmail.com>
2021-05-11 15:01:52 -0400

    Switch Canvas

--- W9D2/Asteroids/dist/main.js
+++ W9D2/Asteroids/dist/main.js
@@ -15,7 +15,7 @@
   \**********************/
 /***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
 
-eval("console.log(\"Webpack is working!\")\n\nconst MovingObject = __webpack_require__(/*! ./moving_object.js */ \"./src/moving_object.js\");\n\nwindow.MovingObject = MovingObject;\n\n\n\n//# sourceURL=webpack:///./src/index.js?");
+eval("console.log(\"Webpack is working!\")\n\nconst MovingObject = __webpack_require__(/*! ./moving_object.js */ \"./src/moving_object.js\");\n\nwindow.MovingObject = MovingObject;\n\ndocument.addEventListener('DOMContentLoaded', function() {\n    const canvas = document.getElementById('game-canvas');\n    const ctx = canvas.getContext('2d');\n});\n\n//# sourceURL=webpack:///./src/index.js?");
 
 /***/ }),
 
@@ -25,7 +25,7 @@ eval("console.log(\"Webpack is working!\")\n\nconst MovingObject = __webpack_req
   \******************************/
 /***/ ((module) => {
 
-eval("function MovingObject(options){\n    this.pos = options['pos'];\n    this.vel = options['vel'];\n    this.radius = options['radius'];\n    this.color = options['color'];\n}\n\n\nMovingObject.prototype.draw = function(ctx){\n    ctx.fillStyle = this.color;\n\n    ctx.beginPath();\n    ctx.arc(this.pos[0],this.pos[1],this.radius,0,2*Math.PI,true);\n    ctx.fill();\n\n}\nconst mo = new MovingObject({\n  pos: [30, 30],\n  vel: [10, 10],\n  radius: 5,\n  color: \"#00FF00\"\n});\n\n\nmodule.exports = MovingObject;\n\n//# sourceURL=webpack:///./src/moving_object.js?");
+eval("function MovingObject(options){\n    this.pos = options['pos'];\n    this.vel = options['vel'];  // [horiz, vert]\n    this.radius = options['radius'];\n    this.color = options['color'];\n}\n\n\nMovingObject.prototype.draw = function(ctx){\n    ctx.fillStyle = this.color;\n\n    ctx.beginPath();\n    ctx.arc(this.pos[0],this.pos[1],this.radius,0,2*Math.PI,true);\n    ctx.fill();\n}\n\n// MovingObject.prototype.move = function(ctx) {\n\n// }\n\n\nconst mo = new MovingObject({\n  pos: [30, 30],\n  vel: [10, 10],\n  radius: 5,\n  color: \"#00FF00\"\n});\n\n// every 1 sec\n\n// at 1 sec :  pos_new [40,40]\n// at 2 sec: pos_new[50, 50]\n\n// canvas = 640 x 480\n// if pos[0] > 640\n// pos[0] = 0\n\n\n// def up\n// vel[1] += 10\n\n\n\nmodule.exports = MovingObject;\n\n\n\n// // Norm([x_1, y_1]) = Dist([0, 0], [x_1, y_1])\n\n// NavigationPreloadManager(vel) = Dist (pos, new_pos)\n\n// // this is math, not JavaScript\n\n// // Dist([x_1, y_1], [x_2, y_2]) = sqrt(  (x_1 - x_2) ** 2 + (y_1 - y_2)** 2)\n// // Dist( pos1, pos2  )\n// [velx, vely]\n\n// Norm ( vel ) = Dist (pos0, pos1)\n\n// Dist(pos0, pos1) = sqrt((pos0[0] - pos1[0]) ** 2 + (pos0[1] - pos1[1]) ** 2)\n\n\n// Norm(vel) = sqrt((pos0[0] - pos1[0]) ** 2 + (pos0[1] - pos1[1]) ** 2)\n\n// Norm(vel) ^ 2 = (pos0[0] - pos1[0]) ** 2 + (pos0[1] - pos1[1]) ** 2\n\n//# sourceURL=webpack:///./src/moving_object.js?");
 
 /***/ })
 
--- W9D2/Asteroids/src/moving_object.js
+++ W9D2/Asteroids/src/moving_object.js
@@ -1,6 +1,6 @@
 function MovingObject(options){
     this.pos = options['pos'];
-    this.vel = options['vel'];
+    this.vel = options['vel'];  // [horiz, vert]
     this.radius = options['radius'];
     this.color = options['color'];
 }
@@ -12,8 +12,13 @@ MovingObject.prototype.draw = function(ctx){
     ctx.beginPath();
     ctx.arc(this.pos[0],this.pos[1],this.radius,0,2*Math.PI,true);
     ctx.fill();
-
 }
+
+// MovingObject.prototype.move = function(ctx) {
+
+// }
+
+
 const mo = new MovingObject({
   pos: [30, 30],
   vel: [10, 10],
@@ -21,5 +26,40 @@ const mo = new MovingObject({
   color: "#00FF00"
 });
 
+// every 1 sec
+
+// at 1 sec :  pos_new [40,40]
+// at 2 sec: pos_new[50, 50]
+
+// canvas = 640 x 480
+// if pos[0] > 640
+// pos[0] = 0
+
+
+// def up
+// vel[1] += 10
+
+
 
 module.exports = MovingObject;
+
+
+
+// // Norm([x_1, y_1]) = Dist([0, 0], [x_1, y_1])
+
+// NavigationPreloadManager(vel) = Dist (pos, new_pos)
+
+// // this is math, not JavaScript
+
+// // Dist([x_1, y_1], [x_2, y_2]) = sqrt(  (x_1 - x_2) ** 2 + (y_1 - y_2)** 2)
+// // Dist( pos1, pos2  )
+// [velx, vely]
+
+// Norm ( vel ) = Dist (pos0, pos1)
+
+// Dist(pos0, pos1) = sqrt((pos0[0] - pos1[0]) ** 2 + (pos0[1] - pos1[1]) ** 2)
+
+
+// Norm(vel) = sqrt((pos0[0] - pos1[0]) ** 2 + (pos0[1] - pos1[1]) ** 2)
+
+// Norm(vel) ^ 2 = (pos0[0] - pos1[0]) ** 2 + (pos0[1] - pos1[1]) ** 2
