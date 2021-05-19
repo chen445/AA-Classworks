d3c37bee9680d40613c090b122b5af53ebb8de47
Ying <cy240705@gmail.com>
2021-05-19 10:37:04 -0700

    weather

--- W10D2/Widgets/frontend/weather.jsx
+++ W10D2/Widgets/frontend/weather.jsx
@@ -8,14 +8,46 @@ class Weather extends React.Component {
         this.success = this.success.bind(this);
     }
 
-componentDidMount() {
-    navigator.geolocation.getCurrentPosition(this.success(position));    
+    componentDidMount() {
+        navigator.geolocation.getCurrentPosition(this.success);    
+    }
+
+    success(position) {
+         fetchWeather(position.coords.longitude,
+                      position.coords.latitude,
+                      weather=>{this.setState({weather})},
+                      error=>{this.setState({error: error})});
+    }
+
+   
+    render(){
+
+        let content = "";
+        if(this.state.weather !==null){
+            let temp=this.state.weather.main.temp;
+            temp=Math.floor((temp - 273.15) * 1.8 + 32);
+            let city=this.state.weather.name;
+            let weather = city+": "+temp+'F';
+            content = (<span>{weather}</span>);
+        
+        }else if(this.state.error){
+            content = (<span>{error}</span>);
+        }else{
+            content = (<span>Fetching...</span>);
+        }
+
+        return (
+            <div>
+                <h1>Weather</h1>
+                {content}
+            </div>
+        );
+        
+    }
+
+
+
 }
 
-success(position) {
-    fetchWeather(position.coords.longitude, position.coords.latitude);
-}
-
-}
 
 export default Weather;
--- W10D2/Widgets/utils/api_util.js
+++ W10D2/Widgets/utils/api_util.js
@@ -1,27 +1,24 @@
   
-export const fetchWeather = (lon, lat) => {
+export const fetchWeather = (lon, lat, success, error) => {
     var request = new XMLHttpRequest();
-    request.open('GET', `http://api.openweathermap.org/data/2.5/weather?lat={${lat}}&lon={${lon}}&appid={f419414a87fe56baee2b3448c7e32ac7}`, true);
-
+    request.open('GET', `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=f419414a87fe56baee2b3448c7e32ac7`, true);
+    
     request.onload = function() {
     if (this.status >= 200 && this.status < 400) {
         // Success!
-        var resp = this.response;
+        var resp = JSON.parse(this.response)
+        
+        success(resp);
     } else {
         // We reached our target server, but it returned an error
-        alert("Location not found")
+        error('Weather is not found');
     }
     };
 
     request.onerror = function() {
     // There was a connection error of some sort
+       error('Fail to fetch data');
     };
 
     request.send();
-
 }
-//     return $.ajax({
-//       method: 'get',
-//       url: 'api.openweathermap.org/data/2.5/weather?q={city name},{state code}&appid={f419414a87fe56baee2b3448c7e32ac7}'
-//     });
-//   };
